9c26d3ae185ff6688f180abfc0720065fd37d587
Brian D. Baker <brian.devs.baker@gmail.com>
2022-02-11 00:30:48 -0500

    Added image upload on backend

---
+++ server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = fileName => {
+  const myFile = fileName.originalname.split('.');
+  const fileType = myFile[myFile.length - 1];
+
+  const imageParams = {
+    Bucket: 'user-images-6b281f5f-b7cd-4527-bc4b-ac650d07ddd0',
+    Key: `${uuidv4()}.${fileType}`,
+    Body: fileName.buffer
+  };
+
+  return imageParams
+};
+
+module.exports = params;
